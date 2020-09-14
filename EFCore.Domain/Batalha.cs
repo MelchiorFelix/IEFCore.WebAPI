ae94830549db292d821dd2ac66a74d18bb86bba2
MelchiorFelix <melchior.alencar@gmail.com>
2020-09-14 06:58:14 -0300

    CRUD em 3 camadas

--- EFCore.Domain/Batalha.cs
+++ EFCore.Domain/Batalha.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace IEFCore.WebAPI.Models
+namespace EFCore.Domain
 {
-    public class Heroi
+    public class Batalha
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public List<Arma> Armas { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DtInicio { get; set; }
+        public DateTime DtFim { get; set; }
         public List<HeroiBatalha> HeroisBatalhas { get; set; }
-        public IdentidadeSecreta IdentidadeSecreta { get; set; }
-
     }
 }
---
+++ IEFCore.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using EFCore.Domain;
+using EFCore.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCore.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public readonly HeroiContext _context;
+        public ValuesController(HeroiContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values/5
+        [HttpGet("AddRange")]
+        public ActionResult GetAddRange()
+        {
+            _context.AddRange(
+                new Heroi { Nome = "Capitão América" },
+                new Heroi { Nome = "Doutor Estranho" },
+                new Heroi { Nome = "Pantera Negra" },
+                new Heroi { Nome = "Viúva Negra" },
+                new Heroi { Nome = "Hulk" },
+                new Heroi { Nome = "Gavião Arqueiro" },
+                new Heroi { Nome = "Capitã Marvel" }
+            );
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet("filtro/{nome}")]
+        public ActionResult Get(string nome)
+        {
+            var herois = _context.Herois.Where(hero => hero.Nome.Contains(nome)).ToList();
+            return Ok(herois);
+        }
+
+        // GET api/values/5
+        [HttpGet("Atualizar/{nameHero}")]
+        public ActionResult Put(string nameHero)
+        {
+
+            var heroi = _context.Herois
+                            .Where(h => h.Id == 3)
+                            .FirstOrDefault();
+            heroi.Nome = "Homem Aranha";            
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public void Delete(int id)
+        {
+            var heroi = _context.Herois.Where(hero => hero.Id == id).Single();
+            _context.Herois.Remove(heroi);
+            _context.SaveChanges();
+        }
+
+    }
+}
